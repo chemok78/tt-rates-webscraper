05bf452180430ec61221c0ae646110a3294de243
Che Mok <chemok78@gmail.com>
2017-03-14 09:25:59 +0000

    height chart from 500px to 800px

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ var margin = {
 
 //width and height of chart, within SVG element
 var w = 1000 - margin.left - margin.right,
-    h = 500 - margin.top - margin.bottom;
+    h = 800 - margin.top - margin.bottom;
     
 var ratesData = "http://rateswebscraper.herokuapp.com/rates";
 
